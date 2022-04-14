504d0fbd8a2787ec3b6b227a787e866c63642879
irine11 <96834447+irine11@users.noreply.github.com>
2022-04-15 01:20:14 +0400

    1 exercise

---
+++ Script.js
@@ -0,0 +1,8 @@
+// 1 exercise
+let array = [5, 25, 89, 120, 36];
+array.push("javascript", "python")
+array.unshift("html", "css")
+console.log(array);
+array.shift();
+array.pop();
+console.log(array);
